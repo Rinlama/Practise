4aafd4e4c928bcecbfe4c0792e96a0c3e855c1b5
class <class@CST_N418_08.cst.citytech.cuny.edu>
2015-02-23 13:23:13 -0500

    List Extra Credit completed

--- src/lecture2/ListPractise.java
+++ src/lecture2/ListPractise.java
@@ -1,25 +1,21 @@
-
-
 package lecture2;
 
 import java.util.*;
 
-
 public class ListPractise {
-    
-    
+
     public static void main(String[] args) {
-        List<String> stringList= new ArrayList<>();
-        
+        List<String> stringList = new ArrayList<>();
+
         stringList.add("Moe");
         stringList.add("Larry");
         stringList.add("Curly");
-        
-        System.out.println("Size : " +stringList.size());
+
+        System.out.println("Size : " + stringList.size());
         System.out.println("get index 0 : " + stringList.get(0));
         System.out.println("get index 1 : " + stringList.get(1));
-        
-        stringList.set(0,"john");
+
+        stringList.set(0, "john");
         stringList.remove(1);
         System.out.println("");
         System.out.println("*********First Print Method :*******");
@@ -27,20 +23,22 @@ public class ListPractise {
             System.out.println(stringList.get(i));
         }
         System.out.println("*****Second  Print Method :*********");
-        for (String name: stringList) {
+        for (String name : stringList) {
             System.out.println(name);
         }
         System.out.println("");
         System.out.println("*****Extra Credit Print:*********");
         System.out.println("*****Third Print Method :*********");
         System.out.println("");
+        //tostring method to print 
         System.out.println(stringList.toString());
         System.out.println("*****Fourth Print Method :*********");
         System.out.println("");
+        //Use Iterator to print
         for (Iterator<String> it = stringList.iterator(); it.hasNext();) {
             String string = it.next();
-            System.out.println(string);   
+            System.out.println(string);
         }
     }
-    
+
 }
